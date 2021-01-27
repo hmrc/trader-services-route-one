b2c275209138d2cf9de3d34fff6b757a423d549e
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2021-01-27 16:35:00 +0000

    Add User-Agent header to outgoing file transfer requests
    Applied via akka HTTP client config - this ensures the header is set on both the GET to upscan (to fetch the file) and the POST to EIS (to transfer the file)

--- app/uk/gov/hmrc/traderservices/controllers/FileTransferFlow.scala
+++ app/uk/gov/hmrc/traderservices/controllers/FileTransferFlow.scala
@@ -104,7 +104,6 @@ trait FileTransferFlow {
               method = HttpMethods.POST,
               uri = appConfig.eisBaseUrl + appConfig.eisFileTransferApiPath,
               headers = collection.immutable.Seq(
-                RawHeader("User-Agent", appConfig.appName),
                 RawHeader("x-conversation-id", fileTransferRequest.conversationId),
                 RawHeader("x-correlation-id", fileTransferRequest.correlationId.getOrElse("")),
                 RawHeader("customprocesseshost", "Digital"),
